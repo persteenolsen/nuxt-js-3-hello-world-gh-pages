4e5ef557643234f2dcaa0da93828deef7531e73c
Per Steen Olsen <persteen22@hotmail.com>
2023-11-23 13:42:53 +0100

    Initial commit

---
+++ app.vue
@@ -0,0 +1,30 @@
+<script setup>
+const version = 2 + 1
+</script>
+
+<template>
+  <div class="hello">
+    Hello World - Nuxt v.{{ version }}.0.0 at GitHub Pages
+  </div>
+   
+ <div class="hellobody">
+   Make sure to take a look at the Readme file before installing :-)
+    
+ </div>
+
+ 
+</template>
+
+<style scoped>
+.hello {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 3rem;
+  padding: 10rem;
+}
+
+.hellobody {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 1rem;
+  padding-left: 10rem;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,7 @@
+export default defineNuxtConfig({
+
+   app: {
+       baseURL: '/nuxt-js-3-hello-world-gh-pages/'
+   
+   }
+})
